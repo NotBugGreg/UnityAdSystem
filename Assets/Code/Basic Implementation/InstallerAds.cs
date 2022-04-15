66bfe51db2bd4d7fa84a5824ecbce6536bf4af66
codegrego <85084249+codegrego@users.noreply.github.com>
2022-04-15 04:49:09 -0400

    commented code

--- Assets/Code/Basic Implementation/InstallerAds.cs
+++ Assets/Code/Basic Implementation/InstallerAds.cs
@@ -1,4 +1,4 @@
-using System;
+/*using System;
 using System.Collections;
 using System.Collections.Generic;
 using Domain;
@@ -101,4 +101,4 @@ public class InstallerAds : MonoBehaviour
     }
 
 }
-
+*/
