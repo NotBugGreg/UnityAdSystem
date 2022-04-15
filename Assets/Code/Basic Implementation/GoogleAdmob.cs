039f17ffed28e07aeb2066afd35e41c06e0d8dd8
codegrego <85084249+codegrego@users.noreply.github.com>
2022-04-15 04:24:44 -0400

    Google admob, IAdPlacement, configuration admob

---
+++ Assets/Code/Basic Implementation/GoogleAdmob.cs
@@ -0,0 +1,165 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using GoogleMobileAds.Api;
+using PlayFab;
+using PlayFab.ClientModels;
+using UnityEngine;
+
+namespace Submodules.UnityAdSystem.Assets.Code.Basic_Implementation
+{
+    public class GoogleAdmob
+    {
+        private RewardedAd _rewardedAd;
+        private readonly string _placementID;
+        private readonly string _rewardID;
+        private readonly string _adID;
+
+        public GoogleAdmob(string placementID, string rewardID, string adID)
+        {
+            _placementID = placementID;
+            _rewardID = rewardID;
+            _adID = adID;
+        }
+
+        public void ConfigurationMobileAds()
+        {
+            MobileAds.Initialize(initStatus => { });
+            List<string> deviceIds = new List<string>();
+            deviceIds.Add("1AC3C6A160DE46279DB08F2BA94DBEEE");
+            RequestConfiguration requestConfiguration = new RequestConfiguration
+                    .Builder()
+                .SetTestDeviceIds(deviceIds)
+                .build();
+            MobileAds.SetRequestConfiguration(requestConfiguration);
+            _rewardedAd = new RewardedAd(_adID);
+
+        }
+
+        public void RequestRewardedAd()
+        {
+            // Create an empty ad request.
+            AdRequest request = new AdRequest.Builder().Build();
+            // Load the rewarded ad with the request.
+            this._rewardedAd.LoadAd(request);
+        }
+
+        public void ConfigureEvents()
+        {
+            // Called when an ad request has successfully loaded.
+            this._rewardedAd.OnAdLoaded += HandleRewardedAdLoaded;
+            // Called when an ad request failed to load.
+            this._rewardedAd.OnAdFailedToLoad += HandleRewardedAdFailedToLoad;
+            // Called when an ad is shown.
+            this._rewardedAd.OnAdOpening += HandleRewardedAdOpening;
+            // Called when an ad request failed to show.
+            this._rewardedAd.OnAdFailedToShow += HandleRewardedAdFailedToShow;
+            // Called when the user should be rewarded for interacting with the ad.
+            this._rewardedAd.OnUserEarnedReward += HandleUserEarnedReward;
+            // Called when the ad is closed.
+            this._rewardedAd.OnAdClosed += HandleRewardedAdClosed;
+        }
+
+        private void HandleRewardedAdLoaded(object sender, EventArgs args)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            PlayFabClientAPI.ReportAdActivity(
+                new ReportAdActivityRequest
+                    {Activity = AdActivity.Start, PlacementId = _placementID, RewardId = _rewardID},
+                result => OnSuccess(result, taskCompletionSource), error => OnError(error, taskCompletionSource));
+            MonoBehaviour.print("HandleRewardedAdLoaded event received");
+            Task.Run(() => taskCompletionSource.Task);
+
+        }
+
+        private void HandleRewardedAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+        {
+            MonoBehaviour.print(
+                "HandleRewardedAdFailedToLoad event received with message: "
+                + args.LoadAdError.GetResponseInfo());
+        }
+
+        private void HandleRewardedAdOpening(object sender, EventArgs args)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            PlayFabClientAPI.ReportAdActivity(
+                new ReportAdActivityRequest
+                    {Activity = AdActivity.Opened, PlacementId = _placementID, RewardId = _rewardID},
+                result => OnSuccess(result, taskCompletionSource), error => OnError(error, taskCompletionSource));
+            MonoBehaviour.print("HandleRewardedAdOpening event received");
+            Task.Run(() => taskCompletionSource.Task);
+
+        }
+
+        private void HandleRewardedAdFailedToShow(object sender, AdErrorEventArgs args)
+        {
+            MonoBehaviour.print(
+                "HandleRewardedAdFailedToShow event received with message: "
+                + args.Message);
+        }
+
+        private void HandleRewardedAdClosed(object sender, EventArgs args)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            PlayFabClientAPI.ReportAdActivity(
+                new ReportAdActivityRequest
+                    {Activity = AdActivity.Closed, PlacementId = _placementID, RewardId = _rewardID},
+                result => OnSuccess(result, taskCompletionSource), error => OnError(error, taskCompletionSource));
+            MonoBehaviour.print("HandleRewardedAdClosed event received");
+            Task.Run(() => taskCompletionSource.Task);
+
+        }
+
+        public void HandleUserEarnedReward(object sender, Reward args)
+        {
+            string type = args.Type;
+            double amount = args.Amount;
+            MonoBehaviour.print(
+                "HandleRewardedAdRewarded event received for "
+                + amount.ToString() + " " + type);
+
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            PlayFabClientAPI.ReportAdActivity(
+                new ReportAdActivityRequest {Activity = AdActivity.End, PlacementId = _placementID, RewardId = _rewardID},
+                result => OnSuccess(result, taskCompletionSource),
+                error => OnError(error, taskCompletionSource));
+            Task.Run(() => taskCompletionSource.Task);
+
+            var rewardAdActivity = new RewardAdActivityRequest
+            {
+                PlacementId = _placementID,
+                RewardId = _rewardID
+            };
+            
+            PlayFabClientAPI.RewardAdActivity(rewardAdActivity, result =>
+            {
+                Debug.Log(result.RewardResults.GrantedItems);
+            }, error => Debug.LogWarning("failed to reward playfab"));
+
+        }
+
+        private void OnError(PlayFabError error, TaskCompletionSource<bool> taskCompletionSource)
+        {
+            taskCompletionSource.SetResult(false);
+            throw new Exception(error.GenerateErrorReport());
+        }
+
+        private void OnSuccess(ReportAdActivityResult result, TaskCompletionSource<bool> taskCompletionSource)
+        {
+            taskCompletionSource.SetResult(true);
+            Debug.Log("result " + result.ToString());
+        }
+
+        public void UserChoseToWatchAd()
+        {
+            if (this._rewardedAd.IsLoaded())
+            {
+                this._rewardedAd.Show();
+            }
+        }
+    }
+}
---
+++ Assets/Code/Basic Implementation/Installers/GlobalInstaller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using PlayFab.ClientModels;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Submodules.UnityAdSystem.Assets.Code.Basic_Implementation.Installers
+{
+    public class GlobalInstaller : MonoBehaviour
+    {
+        public Button showRewardAdButton;
+        private AdPlacementDetails _placement;
+        private void Awake()
+        {
+            showRewardAdButton.onClick.AddListener(InitShowRewardAd);
+        }
+
+        private void InitShowRewardAd()
+        {
+            var adPlacementService = new PlayfabRewardAdsService(PlayfabAdConfiguration.APP_ID_AD,
+                PlayfabAdConfiguration.NAME_ONE_VIDEO_THREE_HINTS_UNIT_ID_TEST);
+            var rewardPlacementAdsUseCase = new RewardPlacementAdsUserCase(adPlacementService);
+            var initializeGame = new InitializeGameUseCase(rewardPlacementAdsUseCase);
+            InitializeGoogleAdmob(initializeGame, adPlacementService);
+        }
+
+        private async void  InitializeGoogleAdmob(InitializeGameUseCase initializeGame, PlayfabRewardAdsService adPlacementService)
+        {
+            var placement = await InitializeGameMethod(initializeGame, adPlacementService);
+            StartGoogleAdmob(placement);
+        }
+        
+        private async Task<AdPlacementDetails> InitializeGameMethod(InitializeGameUseCase initializeGame,
+            PlayfabRewardAdsService playfabRewardAdsService)
+        {
+            await initializeGame.InitGameAsync();
+            var placementsAds = playfabRewardAdsService.Placements;
+            _placement = placementsAds.FirstOrDefault() ??
+                         throw new ArgumentNullException("placementsAds.FirstOrDefault()");
+
+            return _placement;
+        }
+
+
+        private void StartGoogleAdmob(AdPlacementDetails adPlacementDetails)
+        {
+            var googleAdmob = new GoogleAdmob(adPlacementDetails.PlacementId, adPlacementDetails.RewardId, PlayfabAdConfiguration.ONE_VIDEO_THREE_HINTS_UNIT_ID_TEST);
+            googleAdmob.ConfigurationMobileAds();
+            googleAdmob.RequestRewardedAd();
+            googleAdmob.ConfigureEvents();
+            googleAdmob.UserChoseToWatchAd();
+        }
+    }
+}
