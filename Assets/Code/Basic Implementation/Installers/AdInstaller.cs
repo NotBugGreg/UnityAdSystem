a7d8b52d5824cfb5e8d0775d09cf854f88fd7cc6
codegrego <85084249+codegrego@users.noreply.github.com>
2022-05-11 14:06:43 -0400

    enable - events

--- Assets/Code/Basic Implementation/Installers/AdInstaller.cs
+++ Assets/Code/Basic Implementation/Installers/AdInstaller.cs
@@ -21,7 +21,7 @@ namespace Submodules.UnityAdSystem.Assets.Code.Basic_Implementation.Installers
             
             if (_adPlacementDetails != null)
                 GoogleAdmob = new GoogleAdmob(_adPlacementDetails.PlacementId, _adPlacementDetails.RewardId,
-                    PlayfabAdConfiguration.NAME_ONE_VIDEO_THREE_HINTS_UNIT_ID_TEST);
+                    PlayfabAdConfiguration.ONE_VIDEO_THREE_HINTS_UNIT_ID_TEST);
             else
             {
                 throw new SystemException("No adplacements details");
